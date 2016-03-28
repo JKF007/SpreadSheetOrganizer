9d865c400292d1b4a960be7eccb0e80e39c9c65c
JKF007 <jameskfong007@gmail.com>
2016-03-28 04:30:54 -0500

    Revert "Revert "clean up""
    This reverts commit 94155df6c7c6fe3dd1b688d728d88a02fda71df3.

--- src/excelOrganizer.java
+++ src/excelOrganizer.java
@@ -11,6 +11,7 @@ public class excelOrganizer {
 	
 	
 	public static void main(String args[]) throws IOException{
+		
 		ArrayList<String> filteredMainArray  = new ArrayList<String>();
 		
 		excelOrganizer.rowsFilter mainFilter = new excelOrganizer.rowsFilter();
@@ -19,44 +20,36 @@ public class excelOrganizer {
 					
 		filteredMainArray = mainFilter.filterFile(uneditedFile);
 		
-		mainFilter.printResult(filteredMainArray, "editedMainList.csv");
-		
-		
+		rowsFilter.printResult(filteredMainArray, "filteredCoh2Dps.csv");		
 		
 		ArrayList<String> subFilter = new ArrayList<String>();
-		subFilter.add("britInf.txt");
-		subFilter.add("britVeh.txt");
+		
+		
 		subFilter.add("USAInf.txt");
-		subFilter.add("USAVeh.txt");
-		subFilter.add("SovInf.txt");
-		subFilter.add("SovVeh.txt");
 		subFilter.add("wehrInf.txt");
-		subFilter.add("wehrVeh.txt");
+		subFilter.add("SovInf.txt");
 		subFilter.add("okwInf.txt");
+		subFilter.add("britInf.txt");
+		
+		subFilter.add("USAVeh.txt");		
+		subFilter.add("wehrVeh.txt");
+		subFilter.add("SovVeh.txt");
 		subFilter.add("okwVeh.txt");
-		
-		ArrayList<String> subList = new ArrayList<String>();
-		subList.add("britInf.csv");
-		subList.add("britVeh.csv");
-		subList.add("USAInf.csv");
-		subList.add("USAVeh.csv");
-		subList.add("SovInf.csv");
-		subList.add("SovVeh.csv");
-		subList.add("wehrInf.csv");
-		subList.add("wehrVeh.csv");
-		subList.add("okwInf.csv");
-		subList.add("okwVeh.csv");
-		
-		ArrayList<String> filteredSubArray  = new ArrayList<String>();
+		subFilter.add("britVeh.txt");
+				
+		ArrayList<String> sortedArray  = new ArrayList<String>();
 		
 		for (int x = 0 ; x < subFilter.size() ; x++){
 			
 			excelOrganizer.rowsFilter tempFilter = new excelOrganizer.rowsFilter( subFilter.get(x) );
-			filteredSubArray = tempFilter.filterArray(filteredMainArray);
-			tempFilter.printResult(filteredSubArray, subList.get(x));			
+			
+			sortedArray.add( subFilter.get(x) );
+			
+			sortedArray.addAll( tempFilter.filterArray(filteredMainArray) ) ;						
 		}
 		
-		
+		rowsFilter.printResult(sortedArray, "sortedCoh2Dps.csv");
+				
 	}//end of main	
 	
 	static class rowsFilter{
@@ -90,7 +83,7 @@ public class excelOrganizer {
 			return relevantWeapons.contains(weaponName);
 		}
 		
-		public void printResult(ArrayList<String> resultArray, String fileName)throws IOException{
+		public static void printResult(ArrayList<String> resultArray, String fileName)throws IOException{
 			
 			Path destination = Paths.get(fileName);
 			
@@ -131,7 +124,7 @@ public class excelOrganizer {
 			
 			return filteredArray; 
 			
-		}//end of filterFile
+		}//end of method filterArray
 		
 	}//end of class  rowsFilter	
 	
